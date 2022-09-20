5c6cb03c4bf630db2e1f68c6978ceb63f7a93427
thanhhan31 <92517722+thanhhan31@users.noreply.github.com>
2022-09-20 14:46:16 +0700

    Initial commit

---
+++ src/main/java/com/lucistore/lucistorebe/LuciStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.lucistore.lucistorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuciStoreBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucistore/lucistorebe/LuciStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucistore.lucistorebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuciStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
