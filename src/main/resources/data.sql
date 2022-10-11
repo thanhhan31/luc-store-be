8f41367c0a1a16d3bbc5052d2bcb6d761b748dd4
phamphongduycn123 <phamphongduycn123@gmail.com>
2022-10-11 14:21:48 +0700

    default record for buyer rank

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO user_role VALUES ('ADMIN'), ('SALE_ADMIN'), ('BUYER');
 INSERT INTO user_role_permissions VALUES('ADMIN','ADMIN');
+
+-- Buyer rank
+INSERT INTO buyer_rank VALUES (3, 0.1, 'Gold', 10000, null), (2, 0.05, 'Silver', 5000, 3), (1, 0.03, 'Bronze', 1000, 2);
