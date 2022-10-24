3dfc899a7aefd20a924c776b92eeb19d1cf8b24e
phamphongduycn123 <phamphongduycn123@gmail.com>
2022-10-24 17:50:00 +0700

    statistic

--- src/main/java/com/lucistore/lucistorebe/repo/custom/impl/StatisticRepoCustomimpl.java
+++ src/main/java/com/lucistore/lucistorebe/repo/custom/impl/StatisticRepoCustomimpl.java
@@ -80,4 +80,47 @@ public class StatisticRepoCustomimpl implements StatisticRepoCustom{
 		return query.getResultList();
     }
 
+    public List<OrderStatistic> statisticByMonth(List<Long> idProducts,
+            List<Long> idProductVariations,
+            List<Long> idBuyers,
+            List<Long> idAdmins,
+            int year) {
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<OrderStatistic> cq = cb.createQuery(OrderStatistic.class);
+		Root<Order> root = cq.from(Order.class);
+
+		ListJoin<Order, OrderDetail> joinOrderDetail = root.join(Order_.orderDetails, JoinType.LEFT);
+
+		List<Predicate> filters = new ArrayList<>();
+
+
+		if ( idProducts != null) {
+			filters.add(joinOrderDetail.get(OrderDetail_.productVariation).get(ProductVariation_.product).get(Product_.id).in(idProducts));
+		}
+		if ( idProductVariations != null) {
+			filters.add(joinOrderDetail.get(OrderDetail_.productVariation).get(ProductVariation_.id).in(idProductVariations));
+		}
+		if ( idBuyers != null) {
+			filters.add(root.get(Order_.buyer).get(Buyer_.id).in(idBuyers));
+		}
+		if ( idAdmins != null) {
+			filters.add(root.get(Order_.seller).get(User_.id).in(idAdmins));
+		}
+		if ( importDateFrom != null) {
+			filters.add(cb.greaterThanOrEqualTo(root.get(Order_.createTime), importDateFrom));
+		}
+
+		Predicate filter = cb.and(filters.toArray(new Predicate[0]));
+
+		cq.where(filter);
+
+		cq.groupBy(root.get(Order_.id));
+
+		cq.multiselect(cb.sum(joinOrderDetail.get(OrderDetail_.quantity)), joinOrderDetail.get(OrderDetail_.order));
+		
+		TypedQuery<OrderStatistic> query = em.createQuery(cq);
+		return query.getResultList();
+    }
+
 }
