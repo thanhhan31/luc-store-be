e0aae81802471e0faaf5659ca8f338b962495a47
phamphongduycn123 <phamphongduycn123@gmail.com>
2022-10-25 23:02:11 +0700

    statistic

--- src/main/java/com/lucistore/lucistorebe/controller/admin/AdminStatisticController.java
+++ src/main/java/com/lucistore/lucistorebe/controller/admin/AdminStatisticController.java
@@ -4,17 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucistore.lucistorebe.config.login.UserDetailsImpl;
+import com.lucistore.lucistorebe.entity.user.User;
 import com.lucistore.lucistorebe.service.StatisticService;
 
 import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
-@RequestMapping("/api/admi/statistic")
+@RequestMapping("/api/admin/statistic")
 public class AdminStatisticController {
 	@Autowired
 	StatisticService statisticService;
@@ -31,9 +34,9 @@ public class AdminStatisticController {
 			@RequestParam(required = false) Integer month,
 
 			@Parameter(description = "Year to statistic")
-			@RequestParam(required = false) Integer year
+			@RequestParam(required = false) Integer year,
 			
-			// @AuthenticationPrincipal UserDetailsImpl<User> user
+			@AuthenticationPrincipal UserDetailsImpl<User> user
 			){
 
 		var r = statisticService.statistic(idBuyer, idAdmin, month, year, null);
