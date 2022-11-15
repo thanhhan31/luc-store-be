bb1c7d71dd833f3afdf150becd4624299b7d9bb7
thanhhan31 <92517722+thanhhan31@users.noreply.github.com>
2022-11-15 15:43:37 +0700

    Adjust buyer rank 'Bronze' discount to 0 percent

--- src/main/resources/default-data.sql
+++ src/main/resources/default-data.sql
@@ -8,7 +8,7 @@ INSERT IGNORE INTO `user` (`email`, `full_name`, `password`, `phone`, `status`, 
 -- INSERT IGNORE INTO user_role_permissions VALUES('ADMIN','ADMIN');
 
 -- Buyer rank
-INSERT IGNORE INTO buyer_rank VALUES (3, 0.1, 'Gold', 10000, null), (2, 0.05, 'Silver', 5000, 3), (1, 0.03, 'Bronze', 1000, 2);
+INSERT IGNORE INTO buyer_rank VALUES (3, 0.1, 'Gold', 10000, null), (2, 0.05, 'Silver', 5000, 3), (1, 0, 'Bronze', 1000, 2);
 
 -- Product category
 INSERT IGNORE INTO product_category (`level`, `name`, `status`) VALUES ('0', 'Phụ kiện nam', '0');
